ed3983b0143e93968b5888e02860eb2ea642b851
chrisgreen848 <45197319+chrisgreen848@users.noreply.github.com>
2020-03-09 11:55:46 +0000

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,3 +1,22 @@
+
+### ---- Information on how to use -------------
+
+"""
+Enter the file directory in to the 'Enter File Directory text box' and press button
+If you want to add a name file to output names instead of class numbers, do the same for the enter .name File Loc (It will have to be as a text file though'
+Use the class split button to generate class distributions
+
+For test training file option -- This simply checks if there are any missing/corrupt images/ text files in your train file.
+"""
+
+
+
+
+
+
+
+
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -7,6 +26,7 @@ import shutil
 import os
 import cv2
 import v4_training as v4
+from os import path
 
 
 def totalValueOfTally(tally):
@@ -22,6 +42,9 @@ def totalValueOfTally(tally):
 
 
 class dataAnalysis(Widget):
+    defaultPath = "/home/v4/DeepLearning/AlexeyAB/darknet/trainingImages/"
+    defaultClassList = "/home/v4/DeepLearning/AlexeyAB/darknet/cfg/boatcoco.txt"
+    defaultTrainingFile = ""
     bob = 0
     fileDirectory = ""
     textFileTotal = 0
@@ -33,21 +56,29 @@ class dataAnalysis(Widget):
     labelCount = 0
     nameFileLoc = "" #so we can match classes
     classList = []
+    test = "TestTestTestTestTest"
 
     ###------get file directoy entered on the user interface ------
     def getFileDirectory(self, inputText):
-        dataAnalysis.fileDirectory = inputText
+        if inputText != "":
+            dataAnalysis.fileDirectory = inputText
+        else:
+            dataAnalysis.fileDirectory = dataAnalysis.defaultPath
         #print("You have set img directory to : ", dataAnalysis.fileDirectory)
         dataAnalysis.checkFiles(self)
 
 
         ## get name file and added to a list
     def getNameFileLoc(self, inputText):
-        file = open(inputText, "r")
-        for line in file:
-            classLabel = line.rstrip()
-            dataAnalysis.classList.append(classLabel)
-        print(dataAnalysis.classList)
+        try:
+            file = open(inputText, "r")
+            for line in file:
+                classLabel = line.rstrip()
+                dataAnalysis.classList.append(classLabel)
+            print(dataAnalysis.classList)
+        except:
+            print("Opening Textfile failed. Enter new Textfile")
+
 
 
     ##check how many jpg and txt files there are and if there's an even match
@@ -112,9 +143,25 @@ class dataAnalysis(Widget):
 
 
 
+    def checkFalseDetections(self, path):
+        try:
+            file = open(path, "r")
+            for line in file:
+                splitLine = line.split()
+                classTruth = splitLine[1]
+                classDetection = splitLine[2]
+                imgLocation = splitLine[0]
+                if classTruth != classDetection:
+                    # print("falseDetection Class : ", classTruth, " with ", classDetection)
+                    print("falseDetection Class : ", dataAnalysis.classList[int(classTruth)], " with ",
+                          dataAnalysis.classList[int(classDetection)])
+        except:
+            print("File opening error. Please enter new file")
 
 
 
+    #/ home / v4 / DeepLearning / AlexeyAB / darknet / data / falseDecs.txt
+    #/ home / v4 / DeepLearning / AlexeyAB / darknet / cfg / boatcoco.txt
 
 
     ## ---- check if list has number already, if not append -----
@@ -123,7 +170,6 @@ class dataAnalysis(Widget):
         if len(dataAnalysis.classNumbers) == 0:
             dataAnalysis.classNumbers.append(label)
             dataAnalysis.classNumbersTally[0] = 1
-            print("Appended first element")
             ## search array
         else:
             #print("List not empty")
@@ -134,7 +180,7 @@ class dataAnalysis(Widget):
                 if int(label) == int(dataAnalysis.classNumbers[i]):
 
                     seenFlag = 1
-                    p
+
                     dataAnalysis.classNumbersTally[i] = dataAnalysis.classNumbersTally[i] + 1
 
 
@@ -150,14 +196,50 @@ class dataAnalysis(Widget):
         for i in range(len(dataAnalysis.classNumbers)):
 
             classNum = int(dataAnalysis.classNumbers[i])
-            print("CLASS :", dataAnalysis.classList[classNum], " has ", dataAnalysis.classNumbersTally[i], " labels")
+
+            if dataAnalysis.classList != []:
+                print("CLASS :", dataAnalysis.classList[classNum], " has ", dataAnalysis.classNumbersTally[i], " labels")
+            else:
+                print("No Class List has been entered. Displaying NUmbers!")
+                print("CLASS NUMBER : ", dataAnalysis.classNumbers[i], " has ", dataAnalysis.classNumbersTally[i], "Objects present")
 
 
+    def checkTrainingFile(self, txt):
+        it = 0
+        try:
+            file = open(txt, "r")
+            for line in file:
 
+                jpgLocation = line
+                jpgLocation = jpgLocation.rstrip()
+                txtLocation = v4.jpgToTxt(jpgLocation)
 
+                check = path.exists(jpgLocation)
+                if check == False:
+                    print(jpgLocation, "failed")
+
+                check = path.exists(txtLocation)
+                if check == False:
+                    print(txtLocation, "failed")
+
+                ##OPEN TEXT file and check labels
+
+                txtF = open(txtLocation, "r")
+                for label in txtF:
+                    lblSplit = label.split()
+                    if float(lblSplit[1]) > 1.0 or float(lblSplit[2]) > 1.0 or float(lblSplit[3]) > 1.0 or float(
+                            lblSplit[4]) > 1.0:
+                        print("textfile :", txtLocation, "Corrupt")
+
+                # print(path.exists(jpgLocation))
+                # print(path.exists(txtLocation))
+
+                it = it + 1
+        except:
+            print("Failed opening file, try better location")
 
 ##temp name files   /home/v4/DeepLearning/AlexeyAB/darknet/cfg/boatcoco.txt
-##temp image file   /home/v4/Datasets/boats/IndividualClassPhotos/cocoreclassedval/
+##temp image file   /home/v4/Datasets/boats/IndividualClassPhotos/cocoreclassedrelabeled/
 
 
 
