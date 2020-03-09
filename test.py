694e04f27f71ab2d87b11c1241db254e084a854e
chrisgreen848 <45197319+chrisgreen848@users.noreply.github.com>
2020-03-09 11:51:43 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.clock import Clock
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.textinput import TextInput
 import glob
@@ -55,7 +56,7 @@ def getImgList(path):
 
 
 class PongGame(Widget):
-
+    screenMessage = "No Image Selected"
     #variables needed
     currentImg = 0 #position in list
     imgList = []
@@ -83,38 +84,48 @@ class PongGame(Widget):
         PongGame.imgList = getImgList(path)
         try:
             PongGame.imgList.insert(0,PongGame.imgList[0]) # seems to be a bug opening first image, so open it twice ? Fix later
+            PongGame.screenMessage = "Image List Found"
         except:
             print("No images found, or False Directory. Try again")
+            PongGame.screenMessage = "Image List Found not found."
         PongGame.totalImgs = len(PongGame.imgList)
         print("Your list contains : ", PongGame.totalImgs, "Images")
+        #PongGame.screenMessage = "Image List Found not found."
 
     #gets info for next image and stores labels in a list
     def getNextImg(imgList):
-        PongGame.labelList.clear()
-        PongGame.labelListPosition = 0
-        #PongGame.openImg(PongGame.imgList[PongGame.currentImg])
-        PongGame.currentTxtfileLocation = jpgToTxt(PongGame.imgList[PongGame.currentImg]) # stores txt file path for this iteration
-        PongGame.currentJpgFileLocation = PongGame.imgList[PongGame.currentImg] # stores jpg location for this iteration
-
-        print("Current txtfile is :", PongGame.currentTxtfileLocation)
-        img = cv2.imread(PongGame.currentJpgFileLocation)
-        cv2.imshow('img', img)
-        cv2.startWindowThread()
-        cv2.waitKey(10)
-        PongGame.ImgDimensions = img.shape  # get image size
-        print("Current Image is :", PongGame.currentJpgFileLocation)
-        #### Get label List of Img
+        PongGame.screenMessage = "Image List Found not found."
         try:
-            textfile = open(PongGame.currentTxtfileLocation, "r")
-            print("Text file : ", PongGame.currentTxtFileLocation, " is open")
-            for line in textfile:
-                PongGame.labelList.append(line) ### save object label to list
+            PongGame.labelList.clear()
+            PongGame.labelListPosition = 0
+            # PongGame.openImg(PongGame.imgList[PongGame.currentImg])
+            PongGame.currentTxtfileLocation = jpgToTxt(
+                PongGame.imgList[PongGame.currentImg])  # stores txt file path for this iteration
+            PongGame.currentJpgFileLocation = PongGame.imgList[
+                PongGame.currentImg]  # stores jpg location for this iteration
 
-            PongGame.labelListSize = len(PongGame.labelList)
-            print(PongGame.labelList, "Of SIZE", PongGame.labelListSize)
+            print("Current txtfile is :", PongGame.currentTxtfileLocation)
+            img = cv2.imread(PongGame.currentJpgFileLocation)
+            cv2.imshow('img', img)
+            cv2.startWindowThread()
+            cv2.waitKey(10)
+            PongGame.ImgDimensions = img.shape  # get image size
+            print("Current Image is :", PongGame.currentJpgFileLocation)
+            #### Get label List of Img
+            try:
+                textfile = open(PongGame.currentTxtfileLocation, "r")
+                print("Text file : ", PongGame.currentTxtFileLocation, " is open")
+                for line in textfile:
+                    PongGame.labelList.append(line) ### save object label to list
+
+                PongGame.labelListSize = len(PongGame.labelList)
+                print(PongGame.labelList, "Of SIZE", PongGame.labelListSize)
+            except:
+                print("cannot open label file for this image")
+            PongGame.currentImg = PongGame.currentImg + 1
         except:
-            print("cannot open label file for this image")
-        PongGame.currentImg = PongGame.currentImg + 1
+            print("Failed on image : ", PongGame.currentJpgFileLocation)
+
 
     ## path /home/v4/Datasets/boats/NewProposedDataSet/boat_images[val]/
     def labelImg(self):
@@ -165,53 +176,58 @@ class PongGame(Widget):
                 print("Failure to convert")
         else:
             print("No More Labels. Move to Next Image")
+            PongGame.screenMessage = "No More Labels, Move to Next Image"
 
 
     def NewLabelValue(self,text):
-        PongGame.newLabel = text
-        PongGame.filenotfound = 0
-        newTextFileLocation = PongGame.imgList[PongGame.currentImg-1].split("/")
-        tempText = newTextFileLocation[-1].split(".")
-        #print("Temp text at this point is : ", tempText)
-        #print("Position is : ", PongGame.currentImg )
-        tempText[-1] = ".txt"
-        tempText = "".join((tempText))
-        #print("temp text at this poitn is : ", tempText)
-        newTextFileLocation = PongGame.newTextFileLocation + tempText
-        #print("new file location is " , newTextFileLocation)
-        currentLabel = PongGame.labelList[PongGame.labelListPosition-1]
-        currentLabel = currentLabel.split()
-        print(currentLabel[0])
-
-        currentLabel[0] = text
-        newLabel = " ".join(currentLabel)
-        newLabel = newLabel + "\n"
-        print(newLabel)
-
         try:
-            file = open(newTextFileLocation, "a+")
-            #print("File open to be appended")
-            #print("Appending file : ", newTextFileLocation)
-            #print("Image file is : ", PongGame.imgList[PongGame.currentImg-1] )
-            #print("New Label is ", newLabel)
-            try:
-                file.write(newLabel)
-            except:
-                print("Failed to append")
-
-            file.close()
-
+            PongGame.newLabel = text
             PongGame.filenotfound = 0
+            newTextFileLocation = PongGame.imgList[PongGame.currentImg-1].split("/")
+            tempText = newTextFileLocation[-1].split(".")
+            #print("Temp text at this point is : ", tempText)
+            #print("Position is : ", PongGame.currentImg )
+            tempText[-1] = ".txt"
+            tempText = "".join((tempText))
+            #print("temp text at this poitn is : ", tempText)
+            newTextFileLocation = PongGame.newTextFileLocation + tempText
+            #print("new file location is " , newTextFileLocation)
+            currentLabel = PongGame.labelList[PongGame.labelListPosition-1]
+            currentLabel = currentLabel.split()
+            print(currentLabel[0])
 
-        except:
-            print("file not found, create it")
-            PongGame.filenotfound = 1
-            if PongGame.filenotfound == 1:
-                file = open(newTextFileLocation, "w+")
-                print("Created File", newTextFileLocation)
-                file.write(newLabel)
+            currentLabel[0] = text
+            newLabel = " ".join(currentLabel)
+            newLabel = newLabel + "\n"
+            print(newLabel)
+
+
+            try:
+                file = open(newTextFileLocation, "a+")
+                #print("File open to be appended")
+                #print("Appending file : ", newTextFileLocation)
+                #print("Image file is : ", PongGame.imgList[PongGame.currentImg-1] )
+                #print("New Label is ", newLabel)
+                try:
+                    file.write(newLabel)
+                except:
+                    print("Failed to append")
+
                 file.close()
 
+                PongGame.filenotfound = 0
+
+            except:
+                print("file not found, create it")
+                PongGame.filenotfound = 1
+                if PongGame.filenotfound == 1:
+                    file = open(newTextFileLocation, "w+")
+                    print("Created File", newTextFileLocation)
+                    file.write(newLabel)
+                    file.close()
+
+        except:
+            print("Error with label file")
         ##now save new label to required directory
 
     ## path /home/v4/Datasets/boats/NewProposedDataSet/boat_images[val]/
@@ -235,7 +251,8 @@ class PongGame(Widget):
     #def showImage
 
 
-
+    def changeText(self):
+        self.screenMessage = "Changinngngngngngngng"
 
 
 #def callback(instance):
@@ -246,7 +263,7 @@ class PongGame(Widget):
 class PongApp(App):
 
     def build(self):
-
+        #Clock.schedule_interval(self.changeText(), 1.0 / 2.0)
         return PongGame()
 
 
